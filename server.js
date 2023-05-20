a503ae7dc2c0cc0d18b0ab3757d40d2343a72b9f
maruf_mazumder <marufmazumder463@gmail.com>
2023-05-20 19:47:16 +0200

    updated bar chart

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 app.set('view engine', 'ejs');
 const mysql = require('mysql');
 
-const http = require('http');
-const socketIO = require('socket.io');
 
-
-const server = http.createServer(app);
-const io = socketIO(server);
+app.use(express.static(path.join(__dirname, 'public')));
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -23,161 +20,13 @@ const connection = mysql.createConnection({
       return;
     }
     console.log('Connected to MySQL database!');
-    connection.query("SELECT humidity FROM humidity_temperature WHERE(humidity IS NOT NULL) ORDER BY id DESC LIMIT 5" , function(error , humidity){
-      
-      if(error) throw error;
-      console.log(humidity);
-      
-      connection.query("SELECT temperature FROM humidity_temperature WHERE(temperature IS NOT NULL) ORDER BY id DESC LIMIT 5" ,function(error , temperature){
-       
-        if(error) throw error;
-        console.log(temperature);
-        app.get('/', (req, res) => {
-
-          const chartData = {
-            labels: ['1st', '2nd', '3rd', '4th', '5th'],
-            datasets: [{
-              label: 'Humidity',
-              data: humidity.map(obj => obj.humidity),
-              backgroundColor: 'rgba(75, 192, 192, 0.2)',
-              borderColor: 'rgba(75, 192, 192, 1)',
-              borderWidth: 1
-     }]
-          };
-
-
-          const temperatureData = {
-            labels: ['1st', '2nd', '3rd', '4th', '5th'],
-            datasets: [{
-              label: 'temperature',
-              data: temperature.map(temp => temp.temperature),
-              backgroundColor: 'rgba(75, 192, 192, 0.2)',
-              borderColor: 'rgba(75, 192, 192, 1)' ,
-              borderWidth: 1
-            }]
-          };
-
-          res.render('index.ejs', {title : "Temperature - Humidity", holdResult: humidity.concat(temperature) , chartData , temperatureData });
-        });
-
-
-
-        // io.on('connection', (socket) => {
-        //   console.log('Client connected');
-        
-        //   // Example: Sending updated data every second
-        //   setInterval(() => {
-        //     const sqlForHumidity = 'SELECT humidity FROM humidity_temperature WHERE(humidity IS NOT NULL) ORDER BY id DESC LIMIT 5;';
-        //     const sqlForTemperature = 'SELECT temperature FROM humidity_temperature WHERE(temperature IS NOT NULL) ORDER BY id DESC LIMIT 5';
-        //     connection.query(sqlForHumidity, (err, humidity) => {
-        //       if (err) throw err;
-        //       connection.query(sqlForTemperature, (err, temperature) =>{
-
-
-        //         const chartData = {
-        //           labels: ['1st', '2nd', '3rd', '4th', '5th'],
-        //           datasets: [{
-        //             label: 'Humidity',
-        //             data: humidity.map(obj => obj.humidity),
-        //             backgroundColor: 'rgba(75, 192, 192, 0.2)',
-        //             borderColor: 'rgba(75, 192, 192, 1)',
-        //             borderWidth: 1
-        //    }]
-        //         };
-      
-      
-        //         const temperatureData = {
-        //           labels: ['1st', '2nd', '3rd', '4th', '5th'],
-        //           datasets: [{
-        //             label: 'temperature',
-        //             data: temperature.map(temp => temp.temperature),
-        //             backgroundColor: 'rgba(75, 192, 192, 0.2)',
-        //             borderColor: 'rgba(75, 192, 192, 1)' ,
-        //             borderWidth: 1
-        //           }]
-        //         };
-
-        //       socket.emit('dataUpdate', { holdResult: humidity.concat(temperature) , chartData , temperatureData });
-        //       })
-              
-        //     });
-        //   }, 1000);
-        // });
-
-
-        app.listen(3000, () => {
-          console.log('Server listening on port 3000');
-        });
-      })
-
-    });
-
  
   });
 
 
-    io.on('connection', (socket) => {
-          console.log('Client connected');
-        
-          // Example: Sending updated data every second
-          setInterval(() => {
-            const sqlForHumidity = 'SELECT humidity FROM humidity_temperature WHERE(humidity IS NOT NULL) ORDER BY id DESC LIMIT 5;';
-            const sqlForTemperature = 'SELECT temperature FROM humidity_temperature WHERE(temperature IS NOT NULL) ORDER BY id DESC LIMIT 5';
-            connection.query(sqlForHumidity, (err, humidity) => {
-              if (err) throw err;
-              connection.query(sqlForTemperature, (err, temperature) =>{
+    
 
 
-                const chartData = {
-                  labels: ['1st', '2nd', '3rd', '4th', '5th'],
-                  datasets: [{
-                    label: 'Humidity',
-                    data: humidity.map(obj => obj.humidity),
-                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                    borderColor: 'rgba(75, 192, 192, 1)',
-                    borderWidth: 1
-           }]
-                };
-      
-      
-                const temperatureData = {
-                  labels: ['1st', '2nd', '3rd', '4th', '5th'],
-                  datasets: [{
-                    label: 'temperature',
-                    data: temperature.map(temp => temp.temperature),
-                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                    borderColor: 'rgba(75, 192, 192, 1)' ,
-                    borderWidth: 1
-                  }]
-                };
-
-              socket.emit('dataUpdate', { holdResult: humidity.concat(temperature) , chartData , temperatureData });
-              })
-              
-            });
-          }, 1000);
-        });
-
-
-// app.get('/', async (req, res) => {
-
-//     let holdResult = [];
-//     function setHoldResult(data){   
-//         holdResult = data;
-//         console.log("qqqq" , holdResult);
-//         res.render('index', { title: 'Humidity Temperature' , holdResult});
-//     }
-//     holdResult = await connection.query("SELECT * FROM humidity_temperature" , function(error , result){
-//         if(error) throw error;
-//         console.log(result);
-//         setHoldResult(result);
-//     });
-
-// });
-
-// app.listen(3000, () => {
-//   console.log('Server listening on port 3000');
-// });
 
 
 
@@ -237,6 +86,67 @@ client.on('error', function(err) {
 
 
 
+
+app.get('/', async(req, res) => {
+
+  //let humidity = [];
+  //let temperature = [];
+  connection.query("SELECT humidity FROM humidity_temperature WHERE(humidity IS NOT NULL) ORDER BY id DESC LIMIT 5" , function(error , humidity){
+      
+    if(error) throw error;
+    
+    //humidity = humidity;
+    console.log(humidity);
+    
+    connection.query("SELECT temperature FROM humidity_temperature WHERE(temperature IS NOT NULL) ORDER BY id DESC LIMIT 5" ,function(error , temperature){
+     
+      if(error) throw error;
+      
+      //temperature = temperature;
+      console.log(temperature);
+
+      const chartData = {
+        labels: ['1st', '2nd', '3rd', '4th', '5th'],
+        datasets: [{
+          label: 'Humidity',
+          data: humidity.map(obj => obj.humidity),
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          borderColor: 'rgba(75, 192, 192, 1)',
+          borderWidth: 1
+        }]
+      };
+    
+    
+      const temperatureData = {
+        labels: ['1st', '2nd', '3rd', '4th', '5th'],
+        datasets: [{
+          label: 'temperature',
+          data: temperature.map(temp => temp.temperature),
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          borderColor: 'rgba(75, 192, 192, 1)' ,
+          borderWidth: 1
+        }]
+      };
+    
+      res.render('index.ejs', {title : "Temperature - Humidity", holdResult: humidity.concat(temperature) , chartData , temperatureData ,humidity , temperature });
+  
+
+
+    })
+
+
+    
+  });
+
+  
+});
+
+
+
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
 
 
 
